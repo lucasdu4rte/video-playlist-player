e16457d61ce7ed6700b291d13ebb4c804912b472
Lucas Duarte <lucasduarte1112@outlook.com>
2025-05-10 02:02:18 -0300

    Initial Commit

---
+++ VideoPlaylistPlayer/VideoPlaylistPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlaylistPlayerApp.swift
+//  VideoPlaylistPlayer
+//
+//  Created by Lucas Duarte on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlaylistPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlaylistPlayerTests/VideoPlaylistPlayerTests.swift
@@ -0,0 +1,16 @@
+//
+//  VideoPlaylistPlayerTests.swift
+//  VideoPlaylistPlayerTests
+//
+//  Created by Lucas Duarte on 10/05/25.
+//
+
+import Testing
+
+struct VideoPlaylistPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
